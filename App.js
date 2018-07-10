a1f78c1dec7b4cd72eb362f1fa3bf4c7bfce4019
Vittal Pai <vittalpai@ymail.com>
2018-07-10 15:04:54 +0530

    Update Application

--- App.js
+++ App.js
@@ -1,29 +1,45 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Copyright 2018 IBM Corp.
  *
- * @format
- * @flow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, flex} from 'react-native';
+import {WLAuthorizationManager, WLResourceRequest } from 'react-native-ibm-mobilefirst'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  pingMFP() {
+    WLAuthorizationManager.obtainAccessToken("").then(
+      (response) => {
+       console.log('-->  pingMFP(): Success ', response)
+       alert('Connected to MobileFirst Server')
+      }, (error) => {
+       console.log('-->  pingMFP():  ERROR ', error.responseText)
+       alert("Failed to connect to MobileFirst Server")
+      })
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.container} >
+        <Text style={styles.welcome}>Hello MobileFirst</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Ping MFP server" color="white" onPress={this.pingMFP} accessibilityLabel="Press this to ping MFP Server"></Button>
+        </View>
       </View>
     );
   }
@@ -32,18 +48,21 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent:'space-between',
+    alignItems:"center",
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 35,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 80,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  buttonContainer:{
+    display: flex,
+    marginBottom:100,
+    backgroundColor: '#325c80',
+    height: 50,
+    width: 300,
+    alignItems: 'center'
+  }
 });
